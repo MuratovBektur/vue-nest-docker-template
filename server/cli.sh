1924c802a9910ec81409c757eaa211c9b589b243
Bektur Muratov <prostreyd@gmail.com>
2024-05-19 10:39:31 +0600

    fixed  run cli.sh file for local environment

--- server/cli.sh
+++ server/cli.sh
@@ -3,6 +3,6 @@
 # по которому можно обращяться
 source ../docker-compose-name.sh;
 
-DOCKER_COMPOSE_FILE='../docker-compose.dev.yml';
+DOCKER_COMPOSE_FILE='../docker-compose.local.yml';
 
 "${DOCKER_COMPOSE[@]}" -f "$DOCKER_COMPOSE_FILE" exec server "$@"
